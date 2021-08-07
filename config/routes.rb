b291dac9d6629a4b1dfa5b52d3ee719cee257cad
snamiki1212 <snamiki1212@gmail.com>
2021-08-07 10:16:01 -0700

    Add POST #create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      # resources 'calendar', only: [:index, :show, :create]
-      resources 'calendars', only: [:index, :show]
+      resources 'calendars', only: [:index, :show, :create]
     end
   end
 end
