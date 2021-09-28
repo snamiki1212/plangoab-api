2be00dded7e30ada5efb0de20afdafb09e6ea159
snamiki1212 <snamiki1212@gmail.com>
2021-09-28 01:32:47 -0700

    Update converter

--- lib/plangoab/converter.rb
+++ lib/plangoab/converter.rb
@@ -1,43 +1,7 @@
 module Plangoab
   class Converter
-    def initialize(options = {})
-    end
 
-    def self.xxx
-      p "OK"
-    end
-    
-
-    def self.replace_into_attributes_suffix!(obj, params)
-      do_replace_into_attributes_suffix!(obj, params, obj)
-      # puts "<start", obj, params, ">", ""
-      # return if obj.nil?
-      # return if params.nil?
-
-      # params.each do |key, val|
-      #   new_key = "#{key}_attributes".to_sym
-      #   obj[new_key] = obj.delete key
-
-      #   next if val.nil?
-
-      #   if val.is_a? Hash
-      #     puts "<is a", obj, ">", ""
-      #     # debugger
-      #     obj[new_key]&.length&.times do |i|
-      #       self.replace_into_attributes_suffix!(obj[new_key][i], params[key])
-      #     end
-      #     next
-      #   end
-        
-      #   puts "----- throw error"
-      # end
-
-    
-      # obj
-    end
-
-    def self.do_replace_into_attributes_suffix!(obj, params, root)
-      puts "<start", "obj:", obj, "params:", params, "root:", root, ">", ""
+    def self.convert_into_attributes_suffix!(obj, params)
       return if obj.nil?
       return if params.nil?
 
@@ -50,7 +14,7 @@ module Plangoab
 
         if val.is_a? Hash
           obj[new_key]&.length&.times do |i|
-            self.do_replace_into_attributes_suffix!(obj[new_key][i], params[key], root)
+            self.convert_into_attributes_suffix!(obj[new_key][i], params[key])
           end
         end
       end
@@ -59,33 +23,3 @@ module Plangoab
     end
   end
 end
-
-
-  # # ------------------------------
-  # {
-  #   :stories => [
-  #     :reosurces => [
-  #       :events => [
-  #         //...
-  #       ]
-  #     ]
-  #   ]
-  # }
-  
-  # // ------------------------
-  
-  # [
-  #   :stories_attributes => [
-  #     :reosurces_attributes => [
-  #       :events_attributes => [
-  #         //...
-  #       ]
-  #     ]
-  #   ]
-  # ]
-  
-  # // -- // -- // -- // -- // -- // -- // -- //
-  
-  # obj, { stories: {resources: :events} }
-  
-  
--- spec/lib/plangoab/converter_spec.rb
+++ spec/lib/plangoab/converter_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Plangoab::Converter, type: :lib do
 
-  describe '#replace_into_attributes_suffix' do
-    subject { described_class.replace_into_attributes_suffix!(obj, params) }
+  describe '#convert_into_attributes_suffix' do
+    subject { described_class.convert_into_attributes_suffix!(obj, params) }
 
-    # describe do
-    #   let(:obj) { { parent1: nil, parent2: nil } }
-    #   let(:params) { {parent1: nil, parent2: nil} }
-    #   it { is_expected.to include( { parent1_attributes: nil, parent2_attributes: nil } ) }
-    # end
+    describe do
+      let(:obj) { { parent1: nil, parent2: nil } }
+      let(:params) { {parent1: nil, parent2: nil} }
+      it { is_expected.to include( { parent1_attributes: nil, parent2_attributes: nil } ) }
+    end
 
     describe do
       let(:obj) { { stories: [story, story] } }
@@ -30,11 +30,5 @@ RSpec.describe Plangoab::Converter, type: :lib do
         ] },
         ] } ) }
     end
-
-    # describe do
-    #   let(:obj) { {parent: [ {childlen: [ { grand_children: [1]} ]}] } }
-    #   let(:params) { {parent: {childlen: :grand_children} }}
-    #   it { is_expected.to eq(({parent_attributes: [{ children_attributes: [grand_children: [1]] } ]})) }  
-    # end
   end
 end
