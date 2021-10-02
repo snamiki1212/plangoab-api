017996d15fea8574e2212768ffe00d1aed206f64
snamiki1212 <snamiki1212@gmail.com>
2021-10-01 17:47:28 -0700

    Update serializer

--- app/serializers/api/v1/calendar_detail_serializer.rb
+++ app/serializers/api/v1/calendar_detail_serializer.rb
@@ -2,26 +2,5 @@
 
 class Api::V1::CalendarDetailSerializer < ActiveModel::Serializer
   attributes :id
-  # has_many :stories, serializer: Api::V1::StorySerializer
-  
-  
-  has_many :stories
-
-  class StorySerializer < ActiveModel::Serializer
-    attributes :id, :title
-    has_many :resources
-    
-    class ResourceSerializer < ActiveModel::Serializer
-      attributes :id, :title, :order
-      has_many :events
-
-      class EventSerializer < ActiveModel::Serializer
-        attributes :id, :description, :title
-      end
-      
-    end
-    
-  end
-
-
+  has_many :stories, serializer: Api::V1::StorySerializer
 end
