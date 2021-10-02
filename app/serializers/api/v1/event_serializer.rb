d0aecf7adc4f11f479e0dd864710de111a77ac73
snamiki1212 <snamiki1212@gmail.com>
2021-10-01 18:00:18 -0700

    Update serializer

--- app/serializers/api/v1/event_serializer.rb
+++ app/serializers/api/v1/event_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Api::V1::EventSerializer < ActiveModel::Serializer
-  attributes :id, :description, :title
+  attributes :id, :description, :title, :started_at, :ended_at
 end
